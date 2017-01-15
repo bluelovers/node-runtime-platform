d4c4921d0c09a0a6cc85528649d828e4da1ee750
bluelovers <codelovers@users.sourceforge.net>
2017-01-15 12:11:31 +0800

    .

--- test/firefox-demo.js
+++ test/firefox-demo.js
@@ -1,4 +1,11 @@
 const { Cc, Ci, Cu, Cr } = require('chrome');
 
+const URI_PREFIX = (function ()
+{
+	let self = require("sdk/self");
+
+	return ((typeof self.name === 'string') ? self.data.url() : __URI__).replace(/\/[^\/]+\/?$/, '/');
+})();
+
 console.log(require('./lib/runtime'));
 console.log(Cu.import(URI_PREFIX + "lib/runtime.js"));
